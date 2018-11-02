d412fa0e773d1dcb2e76282866be309740cec9b8
dengzhaoxuan <dengzhaoxuan@dangdang.com>
2018-11-02 20:57:37 +0800

    自定义ViewHolder创建过程

--- app/src/main/java/com/study/xuan/emvp/activity/person/PersonModelActivity.java
+++ app/src/main/java/com/study/xuan/emvp/activity/person/PersonModelActivity.java
@@ -1,17 +1,24 @@
 package com.study.xuan.emvp.activity.person;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.study.xuan.emvp.R;
 import com.xuan.annotation.ComponentType;
+import com.xuan.eapi.adapter.ComponentViewAdapter;
+import com.xuan.eapi.adapter.ComponentViewHolderAdapter;
+import com.xuan.eapi.component.Component;
 import com.xuan.eapi.component.IComponentBind;
 import com.xuan.eapi.context.SlotContext;
 import com.xuan.eapi.context.ToolKitBuilder;
+import com.xuan.eapi.factory.IComponentFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,7 +38,7 @@ public class PersonModelActivity extends AppCompatActivity {
         for (int i = 0; i < 100; i++) {
             PersonModel model = new PersonModel();
             model.name = "名字" + i;
-            model.type = new Random().nextInt(5);
+            model.type = new Random().nextInt(10);
             if (model.type == 0) {
                 model.type = 1;
             }
@@ -41,7 +48,18 @@ public class PersonModelActivity extends AppCompatActivity {
         SlotContext slotContext =
                 new ToolKitBuilder<PersonModel>(this)
                         .setData(mData)
-                        .attachRule(PersonModel.class).build();
+                        .attachRule(PersonModel.class)
+                        .setComponentFactory(new IComponentFactory() {
+                            @Override
+                            public Component createViewHolder(Context context, ViewGroup parent,
+                                                              int type) {
+                                if (type > 4) {
+                                    return new ComponentViewHolderAdapter(context,new InnerVH(new TextView(context)));
+                                }
+                                return null;
+                            }
+                        })
+                        .build();
         slotContext.bind(mRcy);
     }
 
