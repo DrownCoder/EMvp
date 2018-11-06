8cdba5290e33074f4724299c889a887c0c624e43
dengzhaoxuan <dengzhaoxuan@dangdang.com>
2018-11-06 21:05:54 +0800

    增加python脚本

---
+++ app/src/main/java/com/study/xuan/emvp/python/SpeedTest.py
@@ -0,0 +1,41 @@
+import os
+
+def createViewHolder(content,fileName):
+	path = '/Users/xuan/Projects/EMvp/app/src/main/java/com/study/xuan/emvp/python'
+
+	if not os.path.exists(path):
+		os.makedirs(path)
+
+	name = fileName + '.java'
+	file = open(name,'w');
+	file.write(content)
+
+	file.close()
+
+	print ('ok')
+
+
+contentCode = "package com.study.xuan.emvp.python;\n" \
+"import android.content.Context;\n" \
+"import android.view.View;\n" \
+"import android.widget.TextView;\n" \
+"import com.xuan.annotation.ComponentType;\n" \
+"import com.xuan.eapi.component.Component;\n" \
+"@ComponentType(\n" \
+"        value = %s,\n" \
+"        view = TextView.class\n" \
+")\n" \
+"public class PyThonVH%s extends Component {\n" \
+"    public PyThonVH%s(Context context, View itemView) {\n" \
+"        super(context, itemView);\n" \
+"    }\n" \
+"    @Override\n" \
+"    public void onBind(int pos, Object item) {\n" \
+"    }" \
+"}"
+
+fileName = 'PyThonVH%s'
+
+
+for i in range(1,10000):
+	createViewHolder(contentCode%((100+i),i,i),fileName%i)
---
+++ app/src/main/java/com/study/xuan/emvp/vh/SimpleVH.java
@@ -0,0 +1,28 @@
+package com.study.xuan.emvp.vh;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.TextView;
+
+import com.xuan.annotation.ComponentType;
+import com.xuan.eapi.component.Component;
+
+/**
+ * Author : xuan.
+ * Date : 2018/11/6.
+ * Description :the description of this file
+ */
+@ComponentType(
+        value = 100,
+        view = TextView.class
+)
+public class SimpleVH extends Component {
+    public SimpleVH(Context context, View itemView) {
+        super(context, itemView);
+    }
+
+    @Override
+    public void onBind(int pos, Object item) {
+
+    }
+}
